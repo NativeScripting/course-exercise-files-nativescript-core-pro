d08fd09812fbdab99ba7603dbdfa0ea8bff33399
Alexander Ziskind <alex@nuvious.com>
2018-05-10 09:33:20 -0400

    chapter 1

---
+++ chapter1/testplatformdeclarations/app/bundle-config.js
@@ -0,0 +1,8 @@
+if (global.TNS_WEBPACK) {
+    // Register tns-core-modules UI framework modules
+    require("bundle-entry-points");
+    // Register application modules
+    // This will register each `root`, `page`, `fragment` postfixed xml, css, js, ts, scss file in the app/ folder
+    var context = require.context("~/", true, /(root|page|fragment)\.(xml|css|js|ts|scss|less|sass)$/);
+    global.registerWebpackModules(context);
+}
